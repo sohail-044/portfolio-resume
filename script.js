bef2b3b4fd9bfb71da6d4055fbbe68d50481aade
sohail044 <mosohail4509851@gmai.com>
2026-07-13 23:15:07 +0530

    Removed folder

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Portfolio Website Loaded!");
+const themeBtn = document.getElementById("theme-btn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeBtn.innerText = "☀️";
+    } else {
+        themeBtn.innerText = "🌙";
+    }
+});
